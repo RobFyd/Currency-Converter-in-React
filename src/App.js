b84f30bf46498e3b524d61f0cd9c8293a0a5d471
Robert Fydrych <123332510+RobFyd@users.noreply.github.com>
2023-04-25 00:37:00 +0100

    Fieldset added

--- src/App.js
+++ src/App.js
@@ -1,16 +1,9 @@
-import FormContainer from "./Form Container";
+import Fieldset from "./Fieldset";
 
 function App() {
   return (
     <form className="form">
-        <fieldset className="form__fieldset">
-            <p className="form__paragraph">Currency Converter</p>
-            <FormContainer />
-            <p>
-                <button className="form__button">Convert</button>
-            </p>
-            <p className="form__result">Result: <strong className="js-result">N/A</strong></p>
-        </fieldset>
+        <Fieldset />
     </form>
   );
 }
---
+++ src/Fieldset/index.js
@@ -0,0 +1,15 @@
+import FormContainer from "../Form Container";
+import "./style.css"
+
+const Fieldset = () => (
+    <fieldset className="form__fieldset">
+        <p className="form__paragraph">Currency Converter</p>
+        <FormContainer />
+        <p>
+            <button className="form__button">Convert</button>
+        </p>
+        <p className="form__result">Result: <strong className="js-result">N/A</strong></p>
+    </fieldset>
+);
+
+export default Fieldset;
