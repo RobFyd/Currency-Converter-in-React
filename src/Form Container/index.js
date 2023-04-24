732149e23aab7a1d387d5f4aa031c535bb557165
Robert Fydrych <123332510+RobFyd@users.noreply.github.com>
2023-04-24 23:50:57 +0100

    Form Container added

---
+++ src/Form Container/index.js
@@ -0,0 +1,33 @@
+import "./style.css"
+
+const FormContainer = () => (
+    <div className="form__container">
+                <div className="form__container--item">
+                    <label className="form__label">
+                        Currency From
+                        <select className="form__field" name="currencyFrom">
+                            <option value="gbp" selected>GBP</option>
+                            <option value="pln">PLN</option>
+                            <option value="eur">EUR</option>
+                        </select>
+                    </label>
+                </div>
+                <div className="form__container--item">
+                    <label className="form__label">
+                        Currency To
+                        <select className="form__field" name="currencyTo">
+                            <option value="gbp">GBP</option>
+                            <option value="pln" selected>PLN</option>
+                            <option value="eur">EUR</option>
+                        </select>
+                    </label>
+                </div>
+                <div className="form__container--item">
+                    <label className="form__label">Amount* <input type="number" min="1" step="any"
+                            className="form__field--amount" required />
+                    </label>
+                </div>
+            </div>
+);
+
+export default FormContainer;
